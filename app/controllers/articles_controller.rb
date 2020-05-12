176d9a2e003c47cd5fe7db98874860fa4925d598
Mirbek Askerov <mirbek.askerov@yandex.ru>
2020-05-13 01:42:14 +0600

    Add: Ability create articles

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,28 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show]
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def show; end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+end
