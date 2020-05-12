0d75e2634fc1413828a60a4ae88f49aa1c5095d6
Mirbek Askerov <mirbek.askerov@yandex.ru>
2020-05-13 05:58:20 +0600

    Add: json serializer for users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :about]
+  before_action :authenticate_user!, except: [:index, :about, :json_data]
 
   def welcome; end
 
   def about; end
+
+  def json_data
+    @users = User.on_created_at
+    render json: @users
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#welcome'
   get 'about', to: 'pages#about'
+  get 'json_data', to: 'pages#json_data'
   resources :articles
   resources :profiles, only: [:index, :show]
 end
