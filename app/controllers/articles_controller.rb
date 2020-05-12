65461d0bf39723ddb22f1dd0128bd168a43541dd
Mirbek Askerov <mirbek.askerov@yandex.ru>
2020-05-13 01:49:24 +0600

    Add: Editing ability

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show]
+  before_action :set_article, only: [:show, :edit, :update]
 
   def new
     @article = Article.new
@@ -16,6 +16,18 @@ class ArticlesController < ApplicationController
   end
 
   def show; end
+
+  def edit; end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated"
+      redirect_to article_path(@article)
+    else
+      flash[:notice] = "Article was not updated"
+      render 'edit'
+    end
+  end
 
   private
     def article_params
