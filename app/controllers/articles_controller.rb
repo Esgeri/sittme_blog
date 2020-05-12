88c43aa0db49b2c21b7c73b229f63344450895de
Mirbek Askerov <mirbek.askerov@yandex.ru>
2020-05-13 01:54:11 +0600

    Add: List of articles & edit/show/back links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update]
 
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
