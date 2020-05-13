7734ad4b77c9e483b048a795ccba73223c4c025b
Mirbek Askerov <mirbek.askerov@yandex.ru>
2020-05-13 07:21:56 +0600

    Add: Feature page specs

---
+++ spec/features/pages/pages_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+feature 'User visit page', %q{
+  In order to be able to visit pages
+  As an user
+  I want to be able to see pages
+} do
+
+  scenario 'User visit root page' do
+    visit root_path
+
+    expect(page).to have_content('Welcome to Blog')
+    expect(page).to have_link('About')
+    expect(current_path).to eq(root_path)
+  end
+
+  scenario 'User visit about page' do
+    visit root_path
+
+    expect(page).to have_link('About')
+    click_on 'About'
+    expect(page).to have_content('About Blog')
+    expect(page).to have_link('Welcome page')
+    expect(page).to have_link('Get json data')
+
+    expect(current_path).to eq(about_path)
+  end
+
+  scenario 'User tries get json data about users' do
+    visit about_path
+
+    expect(page).to have_content('About Blog')
+    expect(page).to have_link('Welcome page')
+    expect(page).to have_link('Get json data')
+
+    click_on 'Get json data'
+
+    expect(page.status_code).to eq(200)
+    expect(page.body).not_to be_nil
+    expect(current_path).to eq(json_data_path)
+  end
+end
