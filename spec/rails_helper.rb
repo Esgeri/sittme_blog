d3cb05e3c7ec5cc8f5674bafbe8e9af61fce6a73
Mirbek Askerov <mirbek.askerov@yandex.ru>
2020-05-13 07:16:02 +0600

    Add: user sign_in spec & login helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,6 +44,8 @@ RSpec.configure do |config|
 
   config.extend ControllerMacros, type: :controller
 
+  config.include LoginMacros, type: :feature
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
